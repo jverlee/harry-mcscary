5aaeef080d05eae573023b5630b9eae22debeae8
Jon <jonverlee@gmail.com>
2021-12-19 19:55:06 -0500

    player and oof

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -2,6 +2,7 @@ import Phaser from 'phaser'
 import ScoreLabel from '../ui/ScoreLabel'
 import BombSpawner from './BombSpawner'
 import StarSpawner from './StarSpawner'
+import Player from './Player'
 
 export default class Game extends Phaser.Scene
 {
@@ -29,6 +30,7 @@ export default class Game extends Phaser.Scene
         this.load.audio("ding", 'assets/sounds/ding.mp3');
         this.load.audio("harry", 'assets/sounds/harry-capture.mp3');
         this.load.audio("tada", 'assets/sounds/tada.mp3');
+        this.load.audio("oof", 'assets/sounds/oof.mp3');
         this.load.spritesheet('dude', 
             'assets/characters/dude.png',
             { frameWidth: 32, frameHeight: 48 }
@@ -42,7 +44,7 @@ export default class Game extends Phaser.Scene
     	// environment, playtforms, players, and score
     	this.add.image(400, 300, 'sky')
     	const platforms = this.createPlatforms()
-    	this.player = this.createPlayer()
+    	this.player = new Player(this).create();
     	this.scoreLabel = this.createScoreLabel(16, 16, 0)
 
     	// stars
@@ -65,6 +67,7 @@ export default class Game extends Phaser.Scene
     	this.ding = this.sound.add("ding", { loop: false });
     	this.harry = this.sound.add("harry", { loop: false });
     	this.tada = this.sound.add("tada", { loop: false });
+    	this.oof = this.sound.add("oof", { loop: false });
 
     }
 
@@ -95,6 +98,9 @@ export default class Game extends Phaser.Scene
 
 	hitBomb(player, bomb)
 	{
+
+		this.oof.play();
+
 		this.physics.pause()
 
 		player.setTint(0xff0000)
@@ -140,36 +146,6 @@ export default class Game extends Phaser.Scene
 
 		// add stars back
 		if (this.starSpawner.countActive() === 0) { this.starSpawner.reset() }
-
-	}
-
-	createPlayer()
-	{
-		const player = this.physics.add.sprite(100, 450, 'dude')
-		player.setBounce(0.2)
-		player.setCollideWorldBounds(true)
-
-		this.anims.create({
-			key: 'left',
-			frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-			frameRate: 10,
-			repeat: -1
-		})
-		
-		this.anims.create({
-			key: 'turn',
-			frames: [ { key: 'dude', frame: 4 } ],
-			frameRate: 20
-		})
-		
-		this.anims.create({
-			key: 'right',
-			frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-			frameRate: 10,
-			repeat: -1
-		})
-
-		return player
 
 	}	
 
---
+++ src/scenes/Player.js
@@ -0,0 +1,43 @@
+import Phaser from 'phaser'
+
+export default class Player
+{
+	/**
+	 * @param {Phaser.Scene} scene
+	 */
+	constructor(scene)
+	{
+		this.scene = scene
+	}
+
+	create()
+	{
+
+		const player = this.scene.physics.add.sprite(100, 450, 'dude')
+		player.setBounce(0.2)
+		player.setCollideWorldBounds(true)
+
+		this.scene.anims.create({
+			key: 'left',
+			frames: this.scene.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+			frameRate: 10,
+			repeat: -1
+		})
+		
+		this.scene.anims.create({
+			key: 'turn',
+			frames: [ { key: 'dude', frame: 4 } ],
+			frameRate: 20
+		})
+		
+		this.scene.anims.create({
+			key: 'right',
+			frames: this.scene.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+			frameRate: 10,
+			repeat: -1
+		})
+
+		return player
+
+	}
+}
